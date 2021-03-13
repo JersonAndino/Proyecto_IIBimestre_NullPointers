9705d811e9448d5b8d43f87b3d038df75fc5583b
Jerson Andino <Jerson Andino@LAPTOP-MP4PIH0R>
2021-03-12 20:09:14 -0500

    Creacion de GUIVenta.

---
+++ src/clases/Administrador.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Administrador extends Persona{
+    private int codigo;
+
+    public Administrador(int codigo, String nombre, String apellido, String cedula) {
+        super(nombre, apellido, cedula);
+        this.codigo = codigo;
+    }
+
+   
+
+ 
+    
+}
