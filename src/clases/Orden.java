ef1a4872b9f2de48abbc5a410593ef86f6103e67
R O G <R O G@DESKTOP-VD4EU9B>
2021-03-14 01:50:10 -0500

    agragar una lista orden

---
+++ src/clases/Orden.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author R O G
+ */
+public class Orden {
+    private String Componente;
+    private int cantidad;
+
+    public Orden(String Componente, int cantidad) {
+        this.Componente = Componente;
+        this.cantidad = cantidad;
+    }
+
+    public String getComponente() {
+        return Componente;
+    }
+
+    public void setComponente(String Componente) {
+        this.Componente = Componente;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
+}
